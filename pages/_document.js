af3cdeffed2094e5215c8241cfe02b6b3352355a
Adrian <eufracioadrian@gmail.com>
2022-08-11 19:44:26 -0700

    updates after code review

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,11 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-
   // To mitigate FOUC, extra tooling to handle styled-components
   // to render CSS 
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
+    const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
@@ -14,17 +13,17 @@ export default class MyDocument extends Document {
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
+        });
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: [initialProps.styles, sheet.getStyleElement()],
       }
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
+  
   render() {
     return (
       <Html lang="en">
